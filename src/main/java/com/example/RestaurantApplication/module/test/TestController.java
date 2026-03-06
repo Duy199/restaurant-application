e55dba8ce5aa933d63c140631d20df04f3f67600
duy <thanhduy.tran3008@gmail.com>
2026-03-06 20:21:28 +0700

    test jenkins

--- src/main/java/com/example/RestaurantApplication/module/test/TestController.java
+++ src/main/java/com/example/RestaurantApplication/module/test/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
     @GetMapping("/sleep")
         public String sleep() throws InterruptedException {
         Thread.sleep(10000);
-        return "ok good";
+        return "ok good ok";
     }
 }
 
